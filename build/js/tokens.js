b8dd4556e386bd6d408ca1dd34b6d482100f2b25
Gabriel Deggau Schmidt <gabriel_d_schmidt@hotmail.com>
2023-06-05 10:07:08 -0300

    initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 05 Jun 2023 13:02:43 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#534c4c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#0b0a0a",
+  "ColorPurple500": "#8257e6"
+};
